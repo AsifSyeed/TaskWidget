252a2ddc16bb24fc88f3d9f8543d0c1171f0f91a
Asif Syeed <asifsyeed666@gmail.com>
2023-09-05 00:19:19 +0600

    Initial Commit

---
+++ TaskWidget/TaskWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskWidgetApp.swift
+//  TaskWidget
+//
+//  Created by Asif Syeed on 5/9/23.
+//
+
+import SwiftUI
+
+@main
+struct TaskWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
